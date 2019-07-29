90ba57bc6f002c14bb26a23b95daa8114735a106
Pato <pato@decentraland.org>
2019-07-29 14:52:54 -0300

    fix: try to force a version increment

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { MoveTransformComponent } from './transform/component/move'
-import { RotateTransformComponent } from './transform/component/rotate'
-import { ScaleTransformComponent } from './transform/component/scale'
-import { FollowPathComponent } from './transform/component/followpath'
-import { KeepRotatingComponent } from './transform/component/keeprotating'
-import { TransformSystem } from './transform/system/transfromSystem'
-import { Interpolate, InterpolationType } from './transform/math/interpolation'
+import { MoveTransformComponent } from './transform/component/move';
+import { RotateTransformComponent } from './transform/component/rotate';
+import { ScaleTransformComponent } from './transform/component/scale';
+import { FollowPathComponent } from './transform/component/followpath';
+import { KeepRotatingComponent } from './transform/component/keeprotating';
+import { TransformSystem } from './transform/system/transfromSystem';
+import { Interpolate, InterpolationType } from './transform/math/interpolation';
 
-import { ToggleComponent, ToggleState } from './toggle/toggleComponent'
+import { ToggleComponent, ToggleState } from './toggle/toggleComponent';
 
 import { Delay } from "./timer/component/delay";
 import { ExpireIn } from "./timer/component/expire";
