6adc7cc3fccefe32ef91e426c015b8384819859c
Esteban Ordano <esteban@decentraland.org>
2020-01-14 23:51:28 -0300

    chore: prettier

--- src/timer/component/delay.ts
+++ src/timer/component/delay.ts
@@ -1,35 +1,34 @@
-import { ITimerComponent } from "./itimercomponent";
-import { TimerSystem } from "../system/timerSystem";
+import { ITimerComponent } from './itimercomponent'
+import { TimerSystem } from '../system/timerSystem'
 
 /**
  * Execute once after X milliseconds
  */
-@Component("timerDelay")
-export class Delay implements ITimerComponent{
-    elapsedTime: number;
-    targetTime: number;
-    onTargetTimeReached: (ownerEntity: IEntity) => void;
+@Component('timerDelay')
+export class Delay implements ITimerComponent {
+  elapsedTime: number
+  targetTime: number
+  onTargetTimeReached: (ownerEntity: IEntity) => void
 
-    private onTimeReachedCallback?: ()=> void
+  private onTimeReachedCallback?: () => void
 
-    /**
-     * @param millisecs amount of time in milliseconds
-     * @param onTimeReachedCallback callback for when time is reached
-     */
-    constructor(millisecs: number, onTimeReachedCallback?: ()=> void){
-        TimerSystem.createAndAddToEngine()
+  /**
+   * @param millisecs amount of time in milliseconds
+   * @param onTimeReachedCallback callback for when time is reached
+   */
+  constructor(millisecs: number, onTimeReachedCallback?: () => void) {
+    TimerSystem.createAndAddToEngine()
 
-        this.elapsedTime = 0
-        this.targetTime = millisecs / 1000
-        this.onTimeReachedCallback = onTimeReachedCallback
-        this.onTargetTimeReached = (entity)=>{
-            if (this.onTimeReachedCallback) this.onTimeReachedCallback()
-            entity.removeComponent(this)
-        }
+    this.elapsedTime = 0
+    this.targetTime = millisecs / 1000
+    this.onTimeReachedCallback = onTimeReachedCallback
+    this.onTargetTimeReached = entity => {
+      if (this.onTimeReachedCallback) this.onTimeReachedCallback()
+      entity.removeComponent(this)
     }
+  }
 
-    setCallback(onTimeReachedCallback: ()=> void){
-        this.onTimeReachedCallback = onTimeReachedCallback
-    }
-
+  setCallback(onTimeReachedCallback: () => void) {
+    this.onTimeReachedCallback = onTimeReachedCallback
+  }
 }
--- src/timer/system/timerSystem.ts
+++ src/timer/system/timerSystem.ts
@@ -1,52 +1,56 @@
-import { ITimerComponent } from "../component/itimercomponent";
-import { Interval } from "../component/interval";
-import { Delay } from "../component/delay";
-import { ExpireIn } from "../component/expire";
+import { ITimerComponent } from '../component/itimercomponent'
+import { Interval } from '../component/interval'
+import { Delay } from '../component/delay'
+import { ExpireIn } from '../component/expire'
 
-export  class TimerSystem implements ISystem {
-    private static _instance: TimerSystem | null = null
+export class TimerSystem implements ISystem {
+  private static _instance: TimerSystem | null = null
 
-    private _components: ComponentConstructor<ITimerComponent>[] = []
+  private _components: ComponentConstructor<ITimerComponent>[] = []
 
-    static createAndAddToEngine(): TimerSystem{
-        if (this._instance == null){
-            this._instance = new TimerSystem()
-            engine.addSystem(this._instance)
+  static createAndAddToEngine(): TimerSystem {
+    if (this._instance == null) {
+      this._instance = new TimerSystem()
+      engine.addSystem(this._instance)
+    }
+    return this._instance
+  }
+
+  static registerCustomComponent<T extends ITimerComponent>(
+    component: ComponentConstructor<T>
+  ) {
+    this.createAndAddToEngine()._components.push(component)
+  }
+
+  private constructor() {
+    TimerSystem._instance = this
+    this._components.push(Interval)
+    this._components.push(Delay)
+    this._components.push(ExpireIn)
+  }
+
+  update(dt: number) {
+    this._components.forEach(component => {
+      this.updateComponent(dt, component)
+    })
+  }
+
+  private updateComponent<T extends ITimerComponent>(
+    dt: number,
+    component: ComponentConstructor<T>
+  ) {
+    let record = engine.getEntitiesWithComponent(component)
+
+    for (const key in record) {
+      if (record.hasOwnProperty(key)) {
+        let entity = record[key]
+        let timerComponent = entity.getComponent(component)
+
+        timerComponent.elapsedTime += dt
+        if (timerComponent.elapsedTime >= timerComponent.targetTime) {
+          timerComponent.onTargetTimeReached(entity)
         }
-        return this._instance
+      }
     }
-
-    static registerCustomComponent<T extends ITimerComponent>(component: ComponentConstructor<T>){
-        this.createAndAddToEngine()._components.push(component)
-    }
-    
-    private constructor(){
-        TimerSystem._instance = this
-        this._components.push(Interval)
-        this._components.push(Delay)
-        this._components.push(ExpireIn)
-    }
-
-    update(dt: number){
-        this._components.forEach(component => {
-            this.updateComponent(dt, component)    
-        });
-    }
-
-    private updateComponent<T extends ITimerComponent>(dt: number, component: ComponentConstructor<T>){
-        let record = engine.getEntitiesWithComponent(component)
-
-        for (const key in record) {
-            if (record.hasOwnProperty(key)) {
-                let entity = record[key];
-                let timerComponent = entity.getComponent(component)
-
-                timerComponent.elapsedTime += dt
-                if (timerComponent.elapsedTime >= timerComponent.targetTime){
-                    timerComponent.onTargetTimeReached(entity)
-                }
-
-            }
-        }
-    }
+  }
 }
